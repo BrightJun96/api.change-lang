7b9635bae7bd0fb68e75ef5f529e045c0de8e690
jjalseu <cjfwns966@gmail.com>
2024-09-14 16:35:37 +0900

    - cors 처리

---
+++ src/main/java/com/example/board/common/config/CorsFilter.java
@@ -0,0 +1,28 @@
+package com.example.board.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsFilter {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+}
--- src/main/java/com/example/board/common/config/SecurityConfig.java
+++ src/main/java/com/example/board/common/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.board.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,12 +17,17 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CorsFilter corsFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(CsrfConfigurer::disable)  // JWT를 사용할 경우 CSRF 보호 비활성화
+                .cors(cors -> cors
+                        .configurationSource(corsFilter.corsConfigurationSource())
+                )
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/login").permitAll()  // 로그인 API는 누구나 접근 가능
                         .anyRequest().authenticated()
